cf8e38b06b23b10dd929ed1dfe898f8bb4e7efaf
Viacheslav Yemets <viacheslav.emets@gmail.com>
2022-04-13 22:21:06 +0300

    create markup for different screens width, add new styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, { useEffect, useState} from "react";
 import './App.css';
 import data from "./data.json";
 import back_icon from "./icons/back.svg";
 import next_icon from "./icons/next.svg";
+import regular_star_icon from "./icons/star-regular.svg";
+import solid_star_icon from "./icons/star-solid.svg";
 
 
 function App() {
@@ -40,6 +42,10 @@ function App() {
     }
     return slicedArr;
   }
+
+  //slice high rate images arr to size described in design doc
+  const slicedHighRateImages = highRateImages.slice(0, 5);
+
   return (
     <div className="app">
       {
@@ -47,37 +53,123 @@ function App() {
           <h1>FEATURED IMAGES</h1>
           <div className="featuredImages_wpap">
             <div className="bigImage">
-              <div className="backArrow" onClick={()=>{
+              <div className="arrow backArrow" onClick={()=>{
                 if(portion === 0) {
                   setPortion(sliceArr(highRateImages).length - 1);
                 } else { setPortion(portion - 1) }
               }}><img src={back_icon} alt="back"/></div>
-              <img src={require(`${mainImg || highRateImages[0].image || spinner}`)} alt="main img"/>
-              <div className="backArrow" onClick={()=>{
+              <div className="addToFavorite"><img src={regular_star_icon} alt="star"/></div>
+              <img src={require(`${(mainImg ? mainImg.image: null) || highRateImages[0].image || spinner}`)} alt="main img"/>
+              <div className="boxShadow"></div>
+              <div className="imgInfo">
+                <div><span className="bigImgTitle">{(mainImg ? mainImg.title : null) || highRateImages[0].title}</span></div>
+                <span className="imgTags">
+                  {(mainImg 
+                    ? mainImg.tags.map((tag, index)=><span key={index}><span>#{tag } </span></span>) 
+                    : null) || highRateImages[0].tags.map((tag, index)=><span key={index}><span>#{tag } </span></span>)}
+                </span>
+              </div>
+            </div>
+            <div className="smallImage">
+              {
+                sliceArr(highRateImages)[portion].map((item, index) => 
+                  <div key={item.id} 
+                       className={ index === 0 ? "smallImages marginBottom" : "smallImages" } 
+                       id="smallImg" 
+                       onClick={()=>{setMainImg(item)}}>
+                    <div className="addToFavorite"><img src={regular_star_icon} alt="star"/></div>
+                    <img src={require(`${item.image}`)} alt={item.title}/>
+                    <div className="boxShadow_small"></div>
+                    <div className="imgInfo">
+                      <div><span className="smallImgTitle">{item.title}</span></div>
+                      {item.tags.map((tag, index)=><span key={index} className="imgTags"><span>#{tag} </span></span>)}
+                    </div>
+                  </div>
+                )
+              }
+              <div className="arrow nextArrow" onClick={()=>{
                 if(portion === sliceArr(highRateImages).length - 1) {
                   setPortion(0)
                 } else { setPortion(portion + 1) }
               }}><img src={next_icon} alt="next"/></div>
             </div>
-            <div>
-              {
-                sliceArr(highRateImages)[portion].map(item => 
-                  <div key={item.id} className="smallImages" id="smallImg" onClick={()=>{setMainImg(item.image)}}>
-                    <img src={require(`${item.image}`)} alt={item.title}/>
-                  </div>
-                )
-              }
+          </div>
+
+          { /* MARKDOWN FOR 680px WIDTH START*/ }
+          <div className="tablet_wpap">
+          <div className="tablet_featuredImages_wpap marginBottom">
+            <div className="bigImage">
+              <img src={require(`${slicedHighRateImages[0].image}`)} alt="main img"/>
+              <div className="boxShadow"></div>
+              <div className="imgInfo">
+                <div><span className="bigImgTitle">{slicedHighRateImages[0].title}</span></div>
+                <span className="imgTags">
+                  {slicedHighRateImages[0].tags.map((tag, index)=><span key={index}><span>#{tag } </span></span>)}
+              </span>
+            </div>
             </div>
           </div>
+
+          <div className="tablet_featuredImages_wrap">
+            {
+              slicedHighRateImages.slice(1, 5).map((item, index) => 
+                <div key={item.id} 
+                    className={ (index === 0 ? "smallImages marginBottom marginRight" : index % 2 === 0 ? "smallImages marginRight" :"smallImages")  } 
+                    id="smallImg">
+                  <div className="addToFavorite"><img src={regular_star_icon} alt="star"/></div>
+                  <img src={require(`${item.image}`)} alt={item.title}/>
+                  <div className="boxShadow_small"></div>
+                  <div className="imgInfo">
+                    <div><span className="smallImgTitle">{item.title}</span></div>
+                    {item.tags.map((tag, index)=><span key={index} className="imgTags"><span>#{tag} </span></span>)}
+                  </div>
+                </div>
+              )
+            }
+          </div>
+          </div>
+
+          { /* MARKDOWN FOR 680px WIDTH END*/ }
+
+          {/* MARKDOWN FOR 280px WIDTH START */}
+
+          <div className="phone_featuredImages_wrap">
+            {
+              slicedHighRateImages.map((item, index) => 
+                <div key={item.id} 
+                    className={ index !== 0 ? "smallImages marginTop" :"smallImages"} 
+                    id="smallImg">
+                  <div className="addToFavorite"><img src={regular_star_icon} alt="star"/></div>
+                  <img src={require(`${item.image}`)} alt={item.title}/>
+                  <div className="boxShadow_small"></div>
+                  <div className="imgInfo">
+                    <div><span className="smallImgTitle">{item.title}</span></div>
+                    {item.tags.map((tag, index)=><span key={index} className="imgTags"><span>#{tag} </span></span>)}
+                  </div>
+                </div>
+              )
+            }
+          </div>
+            
+          {/* MARKDOWN FOR 280px WIDTH END */}
+
           <div>
             <h1>LAST IMAGES</h1>
-            <div>
+            <div className="smallImages_flex">
               {
-                newAgeImages.map(item => 
-                  <div key={item.id} className="smallImages">
-                    <div><img src={require(`${item.image}`)} alt={item.title}/></div>
+                newAgeImages.map((item, index) => 
+                  <div key={item.id} >
+                    <div className={ index !== 0 ? "smallImages marginRight marginTop" : "smallImages marginRight"}>
+                      <img src={require(`${item.image}`)} alt={item.title}/>
+                      <div className="addToFavorite"><img src={regular_star_icon} alt="star"/></div>
+                    </div>
+                    <div>
+                      <div className="smallImgTitle_wrap"><span className="smallImgTitle">{item.title}</span></div>
+                      <span>{item.tags.map((tag, index)=><span key={index} className="newImgTags"><span>#{tag} </span></span>)}</span>
+                    </div>
                   </div>)
               }
+              <div className="banner"><div>BANNER</div></div>
             </div>
           </div>
         </div>
@@ -87,5 +179,3 @@ function App() {
 }
 
 export default App;
-
-//<img src={require(`${response[0].image}`)} alt="dc"/>
